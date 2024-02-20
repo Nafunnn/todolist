b612cf64be156312519c787828749a4712323d13
Nafunnn <aanalim31@gmail.com>
2024-02-20 11:28:28 +0700

    First commit

---
+++ resources/views/auth/reset.blade.php
@@ -0,0 +1,67 @@
+<!-- reset.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div style="text-align: center">
+                <h1 style="">My ToDo</h1>
+                <p>Don't let busyness beat you. Manage your time and prioritize wisely through your list.</p>
+            </div>
+            <div class="card">
+                <div class="card-header">Reset Password</div>
+
+                <div class="card-body">
+                    <form action="/reset/password/store" method="POST">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" required>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Input field untuk password -->
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('New Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Input field untuk konfirmasi password -->
+                        <div class="row mb-3">
+                            <label for="password_confirmation" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Change Password') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
